337661cc7fd43bca5ec71eec84e06cb3d6619fb2
jkremes <only1truth333@gmail.com>
2018-11-20 16:16:53 -0500

    Updates

---
+++ precache-manifest.0add280492b6c721d6b264cfd1c01750.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "96a5bf6530f10e9e492a",
+    "url": "/jkremes/GoT-Houses/static/js/runtime~main.96a5bf65.js"
+  },
+  {
+    "revision": "f10d8bba47046fe2e3ac",
+    "url": "/jkremes/GoT-Houses/static/js/main.f10d8bba.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/jkremes/GoT-Houses/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "f10d8bba47046fe2e3ac",
+    "url": "/jkremes/GoT-Houses/static/css/main.c9c6ef91.chunk.css"
+  },
+  {
+    "revision": "bf6c65846cd53df1340725807e550ee4",
+    "url": "/jkremes/GoT-Houses/index.html"
+  }
+];
